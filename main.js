190901ecc28505aa4ed38c127c4ab03bbd1d9e52
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2023-06-11 17:45:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,21 +4,17 @@ function updateClock() {
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
 
-    // Agrega un cero inicial si los minutos o segundos son menores a 10
     minutes = (minutes < 10 ? "0" : "") + minutes;
     seconds = (seconds < 10 ? "0" : "") + seconds;
 
-    // Formato de 12 horas o 24 horas (dependiendo de tus preferencias)
     var amPm = hours < 12 ? "AM" : "PM";
     hours = hours > 12 ? hours - 12 : hours;
     hours = hours === 0 ? 12 : hours;
 
-    // Actualiza el contenido del reloj
     var clockElement = document.getElementById("clock");
     clockElement.innerHTML = hours + ":" + minutes + ":" + seconds + " " + amPm;
   }
 
-  // Actualiza el reloj cada segundo
   setInterval(updateClock, 1000);
 
 
